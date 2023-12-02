29d075cef1e8854bf93600ba0913596d8f1c4ff6
Nelson Silva <nelsonsilvadev@gmail.com>
2023-12-02 04:02:13 +0000

    Add Ubuntu font from Google Fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang="en" className="h-full antialiased">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <body className="flex min-h-full flex-col">
         <Main />
         <NextScript />
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#1a2b33',
+        secondary: '#f8f9f2',
+        current: 'currentColor',
+      },
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
 export default config
